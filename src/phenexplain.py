cf29657a6c5ac5bd384d30d3c0b0c90d75cb92ee
Alexis Lamiable <alexis@lamiable.fr>
2023-01-18 11:33:41 +0100

    fixed a bug with methods 1 & 2, in "single" mode

--- src/phenexplain.py
+++ src/phenexplain.py
@@ -29,49 +29,48 @@ def single(stylegan_weights, clazz, class_idxs, output=None,
         os.makedirs(outputdir)
 
         class_oh   = F.one_hot(torch.tensor(class_idxs), num_classes=G.c_dim).type(torch.DoubleTensor).to(gpu)
+        C_start = class_oh[0:1, :]
+        C_end = class_oh[1:2, :]
+        E_start = G.mapping.embed(C_start)
+        E_end = G.mapping.embed(C_end)
+        if method == 2:
+            W_center = []
+            for i in range(class_oh.shape[0]):
+                W = G.mapping(torch.randn([100000, G.z_dim]).to(gpu),
+                              class_oh[i].repeat(100000,1))
+                W_center.append(torch.mean(W,axis=0))
+            W_center = torch.stack(W_center)
+            W_shift = W_center - W_center[0]
 
         for k in range(samples):
             Z = torch.randn([1, G.z_dim]).to(gpu)
-            # torch.save(Z, os.path.join(outputdir, 'z_%02d.pt' % k))
-            W = G.mapping(Z.repeat(class_oh.shape[0], 1), class_oh)
-
+            
             Wj = []
             if method == 3:
-                # straight lines	
+                # straight lines
+                W = G.mapping(Z.repeat(class_oh.shape[0], 1), class_oh)
                 W_start = W[0]
                 W_end   = W[1]
-			
-                Wj = []
-                for j in range(steps):	
-                    Wj.append(torch.lerp(W_start, W_end, j / (steps-1)))
-            elif method == 1:
-                E_start = G.mapping.embed(C_start)
-                E_end = G.mapping.embed(C_end)
 
+                for j in range(steps):
+                    Ej = torch.lerp(W_start, W_end, j / (steps-1))
+                    Wj.append(Ej)
+            elif method == 1:
                 G.mapping.embed = Identity()
                 G.mapping.c_dim = 512
 
                 for j in range(steps):	
                     Ej = torch.lerp(E_start, E_end, j / (steps-1))
-                    Wj.append(G.mapping(Z, Ej))
+                    Wj.append(G.mapping(Z, Ej)[0])
             elif method == 2:
-                W_center = []
-                for i in range(class_oh.shape[0]):
-                    W = G.mapping(torch.randn([100000, G.z_dim]).to(gpu),
-                                  class_oh[i].repeat(100000,1))
-                    W_center.append(torch.mean(W,axis=0))
-                W_center = torch.stack(W_center)
-
                 W_start = G.mapping(Z, C_start)
-                W_shift = W_center - W_center[0]
+                W_end   = W_start + W_shift
 
-                W_end   = W_start + W_shift.repeat(n_samples,1,1)
-
-                for j in range(steps):	
-                    Wj.append(torch.lerp(W_start, W_end, j / (steps-1)))
+                for j in range(steps):
+                    wjtmp = torch.lerp(W_start, W_end, j / (steps-1))
+                    Wj.append(wjtmp[1])
 	    
             Wj = torch.stack(Wj)
-	    
             imgs = G.synthesis(Wj, noise_mode='const', force_fp32=fp32)
             _min = torch.min(imgs)
             _max = torch.max(imgs)
